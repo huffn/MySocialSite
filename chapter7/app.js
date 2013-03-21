0e4962f8606473145683aa80261faf63e398886a
Nathaniel Huff <nathan.r.huff@gmail.com>
2013-03-21 16:12:04 -0600

    Continued working through chapter 7

--- chapter7/app.js
+++ chapter7/app.js
@@ -71,6 +71,15 @@ app.get('/account/authenticated', function(req, res){
    }
 });
 
+app.get('/accounts/:id', function(req, res){
+   var accountId = req.params.id == 'me'
+      ? req.session.accountId
+      : req.params.id;
+   Account.findOne({_id:accountId}, function(account){
+      res.send(account);
+   });
+});
+
 app.post('/forgotpassword', function(req, res){
    var hostname= req.headers.host;
    var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
